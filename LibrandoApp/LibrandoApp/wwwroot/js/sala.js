388ef7c4e43c52f33b70ddc729be4b74ed265cc8
milenaff <mih,ferreiradepaula@gmail.com>
2021-04-26 22:00:46 -0300

    Chamada API Sala pelo front

---
+++ LibrandoApp/LibrandoApp/wwwroot/js/sala.js
@@ -0,0 +1,24 @@
+﻿function criarSala() {
+
+   var url = "https://librando.azurewebsites.net/api/sala/register";
+
+    var sala = {};
+    sala.roomName = $('#exampleDropdownFormNome').val();
+    
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: sala,
+        cache: false
+    })
+        .done(function (data) {
+            
+            alert("sala cadastrada");
+            console.log("sala cadastrada");
+
+        }).fail(function (XMLHttpRequest, textStatus, errorThrown) {
+            
+            console.log(errorThrown);
+        });
+
+}
