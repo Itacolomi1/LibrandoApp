245007eba477ce4867f6fa2b894a7c9d2c8fea4c
Ítalo Martins <italo.mtr@gmail.com>
2021-05-30 15:46:56 -0300

    validação feita pelo Sala Ativa

--- LibrandoApp/LibrandoApp/wwwroot/js/salaEspera.js
+++ LibrandoApp/LibrandoApp/wwwroot/js/salaEspera.js
@@ -9,22 +9,48 @@ function mostrarSalaEspera() {
 
 function direciona_jogo() {
 
-    if (localStorage.getItem('status_sala') == 0) {
-        alert("Espere a responsável liberar o jogo");
-    }
-    else {
-        if (localStorage.getItem('tipo_jogo') == 'Meteoro') {
-            window.location = "https://librandoapp.azurewebsites.net/Jogo2";
-        }
-        else {
-            window.location = "https://librandoapp.azurewebsites.net/PrimeiroJogo";
-        }
+    var sala = new Object();
+    sala.roomname = localStorage.getItem('nome_sala');
+    var url = 'https://librando.azurewebsites.net/api/sala/sala_ativa';
+    //var url = `http://localhost:9090/api/sala/sala_ativa`;
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: sala,
+        cache: false
+    })
+        .done(function (data) {
+
+            if (!sala) {
+                alert("Espere a responsável liberar o jogo");
+            }
+            else {
+                if (localStorage.getItem('tipo_jogo') == 'Meteoro') {
+                    window.location = "https://librandoapp.azurewebsites.net/Jogo2";
+                }
+                else {
+                    window.location = "https://librandoapp.azurewebsites.net/PrimeiroJogo";
+                }
+
+            }
+
+
+        }).fail(function (XMLHttpRequest, textStatus, errorThrown) {
+
+            alert("Erro ao validar status da sala");
+            console.log(errorThrown);
+        });
 
 
 
 
-    }
-        
+
+
+
+
+
+   
     
 
 }
