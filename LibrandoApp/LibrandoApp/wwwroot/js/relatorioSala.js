f77826c59c5cde0c00cd1312d8dd8eba2a6032d0
patrcastro <patrcastro@hotmail.com>
2021-05-29 21:18:11 -0300

    Criação do relatorio para visualização dos pontos de cada aluno

--- LibrandoApp/LibrandoApp/wwwroot/js/relatorioSala.js
+++ LibrandoApp/LibrandoApp/wwwroot/js/relatorioSala.js
@@ -1,21 +1,24 @@
 ﻿$(document).ready(function () {
+    var token = localStorage.getItem('user_token').replaceAll("\"", "");
 
-    var token = localStorage.getItem('user_token');
+    const urlParams = new URLSearchParams(window.location.search);
+    var id = urlParams.get('Id');
 
     $('#tabelaRelatorio').DataTable({
         dom: "Bfrtip",
         ajax: {
-            // url: "https://librando.azurewebsites.net/api/sala/",
-            url: "http://localhost:9090/api/sala/",
-            type: 'GET',
+            // url: "https://librando.azurewebsites.net/api/sala/jogadores",
+            url: "http://localhost:9090/api/sala/jogadores",
+            type: 'POST',
+            data: { _id: id },
             headers: {
                 Authorization: 'Bearer ' + token
             },
             "dataSrc": '',
         },
         columns: [
-            { data: "cod_acesso" },
-            { data: "roomName" },
+            { data: "personName" },
+            { data: "pontos" },
         ],
     });
 });
